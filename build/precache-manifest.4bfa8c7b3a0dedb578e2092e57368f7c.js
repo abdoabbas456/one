a69edd37c1233f2b239d7a09ffeeb3bc025711c0
abdoabbas456 <62522182+abdoabbas456@users.noreply.github.com>
2020-10-08 17:46:03 +0300

    Add files via upload

---
+++ build/precache-manifest.4bfa8c7b3a0dedb578e2092e57368f7c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7186963e6a85087accc44bba78ae291",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15236ffcd079cae4f4ca",
+    "url": "/static/css/main.b07e137b.chunk.css"
+  },
+  {
+    "revision": "a60fc69a2ab9d9d88488",
+    "url": "/static/js/2.f2e35bae.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/static/js/2.f2e35bae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15236ffcd079cae4f4ca",
+    "url": "/static/js/main.39f54f94.chunk.js"
+  },
+  {
+    "revision": "8490ead4ccb4d98d7852",
+    "url": "/static/js/runtime-main.cd377bed.js"
+  },
+  {
+    "revision": "32b847c5e4afdda668f1d04e72688498",
+    "url": "/static/media/Mask Group 3.32b847c5.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/bars.b3f49afd.svg"
+  },
+  {
+    "revision": "9d7f51056f54c2b79b47fa7f52d3f195",
+    "url": "/static/media/card1.9d7f5105.svg"
+  },
+  {
+    "revision": "8a7a392a7022fc48dd3e49aad5eff3c5",
+    "url": "/static/media/card2.8a7a392a.svg"
+  },
+  {
+    "revision": "e061caf48bab3537b49dd7717f221f2a",
+    "url": "/static/media/card3.e061caf4.svg"
+  },
+  {
+    "revision": "26306a76feb49cae0253ce04b66e5ca8",
+    "url": "/static/media/hero1.26306a76.jpg"
+  },
+  {
+    "revision": "78f3123c6bd170fff0464868be4d6de0",
+    "url": "/static/media/search.78f3123c.svg"
+  }
+]);
